3504ce183467c08f10dfe592efbb5c3c973cb539
rehanmm <21bcs049@nith.ac.in>
2026-01-27 00:11:57 +0530

    fixed minor error

--- src/services/VendorService.js
+++ src/services/VendorService.js
@@ -21,7 +21,7 @@ export const fetchVendorReportData = async (vendorName, fromDate, toDate) => {
 
   try {
     const response = await fetch(url, {
-      method: 'GET',
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
